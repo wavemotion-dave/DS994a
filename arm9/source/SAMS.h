667b2addaaae9de491dd74e9f46bc303252efd47
Dave Bernazzani <meeplemaster@gmail.com>
2024-12-28 11:16:16 -0500

    Version 2.5 with cleanup across the board.

--- arm9/source/SAMS.h
+++ arm9/source/SAMS.h
@@ -1,11 +1,13 @@
 // =====================================================================================
-// Copyright (c) 2023-2024 Dave Bernazzani (wavemotion-dave)
+// Copyright (c) 2023-2025 Dave Bernazzani (wavemotion-dave)
 //
-// Copying and distribution of this emulator, its source code and associated 
-// readme files, with or without modification, are permitted in any medium without 
+// Copying and distribution of this emulator, its source code and associated
+// readme files, with or without modification, are permitted in any medium without
 // royalty provided this copyright notice is used and wavemotion-dave is thanked profusely.
 //
 // The DS994a emulator is offered as-is, without any warranty.
+//
+// Please see the README.md file as it contains much useful info.
 // =====================================================================================
 #ifndef _SAMS_H_
 #define _SAMS_H_
